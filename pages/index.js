b8e2322aa7cf50319f3b7d2b9e9003b5a1d41f27
johncorrigan1 <jcorrigan322@gmail.com>
2022-10-08 15:33:09 -0400

    test

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Nala from './../public/Nala.jpeg'
 
 
 export default function Home() {
   return (
     <div className='flex justify-center flex-col items-center gap-10'>
-      <h1 className='text-2xl text-rose-600 text-center mt-10'>Coming Soon...</h1>
+      <h1 className='text-2xl text-rose-600 text-center mt-10'>Coming Soon to a Web Browser near you...</h1>
       <Image 
-      src="/../public/Nala.jpeg"
+      src={Nala}
       alt="epic doggo"
       height={750}
       width={750} />
