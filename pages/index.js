4850b7a74cbae96c90a6d51623f0322b7d285472
johncorrigan1 <jcorrigan322@gmail.com>
2022-10-09 17:17:56 -0400

    improving seo

--- pages/index.js
+++ pages/index.js
@@ -8,13 +8,18 @@ import Header from './components/ui/Header'
 export default function Home() {
   return (
     <div className='flex justify-center flex-col items-center gap-10'>
+      <Head>
+      <title>John Corrigan</title>
+      <meta name="description" content="My personal portfolio website" />
+      <link rel='icon' href="/favicon.ico" /> 
+      </Head>
       <Header />
       <About />
       <h1 className='text-2xl text-rose-600 text-center mt-10'>Coming Soon to a Web Browser near you...</h1>
 
       <Image 
       src={Nala}
-      alt="epic doggo"
+      alt="My dog looking funny"
       height={500}
       width={500} />
     </div>
