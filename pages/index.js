7b4624abebf6933372a15db2c0f6112233175def
johncorrigan1 <jcorrigan322@gmail.com>
2022-10-08 20:34:29 -0400

    added header

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,19 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Nala from './../public/Nala.jpeg'
+import Header from './components/ui/Header'
 
 
 export default function Home() {
   return (
     <div className='flex justify-center flex-col items-center gap-10'>
+      <Header />
       <h1 className='text-2xl text-rose-600 text-center mt-10'>Coming Soon to a Web Browser near you...</h1>
       <Image 
       src={Nala}
       alt="epic doggo"
-      height={750}
-      width={750} />
+      height={500}
+      width={500} />
     </div>
   )
 }
