0bc76bee1ed73cecaeb2bb762397004965ee9121
John Corrigan <jcorrigan322@gmail.com>
2022-12-21 21:47:36 -0500

    skills

--- components/Icon.jsx
+++ components/Icon.jsx
@@ -1,7 +1,7 @@
 const Icon = (props) => {
   return (
     <img
-      className="w-24 hover:scale-110 duration-300"
+      className="w-24 hover:scale-110 duration-300 hover:rotate-12"
       src={`https://skillicons.dev/icons?i=${props.icon}&perline=1`}
       height={50}
       width={50}
